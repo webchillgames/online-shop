0f34f49e1f1f72c736a11aa3ca7879d0e63693ff
webchillgames <web.chill.games@gmail.com>
2023-05-19 11:19:55 +0800

    draft

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === "production" ? "/online-shop/" : "/",
+  base: "/online-shop/",
+
+  // base: process.env.NODE_ENV === "production" ? "/online-shop/" : "/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
